c8dd1ec3f1b2556888631b8a41a272afe199d085
rehank703 <rehanamaankhanh@gmail.com>
2025-08-13 20:29:40 +0530

    Add files via upload

---
+++ inventary manage/funct.py
@@ -0,0 +1,8 @@
+#to add product
+def addpro(product, price, quantit):
+    print( "PRODUCT: ", product,"\nPRICE: ", price,"\nQUANTITY:", quantit)
+    print("product added succesfully") 
+    
+
+# to update the product
+
